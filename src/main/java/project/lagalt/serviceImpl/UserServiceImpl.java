a533cb9f5a37fc9701a9d86de4bb88243ae9b999
98emre <emre-98@live.se>
2023-08-29 14:06:05 +0200

    feat: added user service implement setup

---
+++ src/main/java/project/lagalt/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package project.lagalt.serviceImpl;
+
+import org.springframework.stereotype.Service;
+import project.lagalt.service.UserService;
+
+import java.util.Collection;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public Collection findAll() {
+        return null;
+    }
+
+    @Override
+    public Object findById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object add(Object entity) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object entity) {
+        return null;
+    }
+
+    @Override
+    public Object deleteById(Object o) {
+        return null;
+    }
+}
