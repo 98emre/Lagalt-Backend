f657e713551059ef90c26f5eef0a2b6a813cd757
98emre <emre-98@live.se>
2023-08-29 16:10:49 +0200

    feat: so we can make enum uppercase

--- src/main/java/project/lagalt/utilites/enums/Skills.java
+++ src/main/java/project/lagalt/utilites/enums/Skills.java
@@ -1,5 +1,18 @@
 package project.lagalt.utilites.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Skills {
-    JAVA, JAVASCRIPT, REACT, ANGULAR, C
+    JAVA, JAVASCRIPT, REACT, ANGULAR, C;
+
+    @JsonValue
+    public String toValue(){
+        return this.name();
+    }
+
+    @JsonCreator
+    public static Skills forValue(String value){
+        return Skills.valueOf(value.toUpperCase());
+    }
 }
