8a705971ae935f8af77803376f5daa1f84b9f8fd
98emre <emre-98@live.se>
2023-08-30 14:51:15 +0200

    feat: created collaborator mapper

---
+++ src/main/java/project/lagalt/mapper/CollaboratorMapper.java
@@ -0,0 +1,23 @@
+package project.lagalt.mapper;
+
+import org.mapstruct.Mapper;
+import project.lagalt.model.dtos.collaborator.CollaboratorDTO;
+import project.lagalt.model.dtos.collaborator.CollaboratorPostDTO;
+import project.lagalt.model.dtos.collaborator.CollaboratorUpdateDTO;
+import project.lagalt.model.entities.Collaborator;
+
+import java.util.Collection;
+
+@Mapper(componentModel = "spring")
+public abstract class CollaboratorMapper {
+
+    public abstract Collaborator collaboratorPostDtoToCollaborator(CollaboratorPostDTO collaboratorPostDTO);
+
+    public abstract Collaborator collaboratorUpdateDtoToCollaborator(CollaboratorUpdateDTO collaboratorUpdateDTO);
+
+    public abstract CollaboratorDTO collaboratorDTOtoCollabor(Collaborator collaborator);
+
+    public abstract Collection<CollaboratorDTO> collaboratorToCollaboratorDtos(Collection<Collaborator> collaborators);
+
+
+}
