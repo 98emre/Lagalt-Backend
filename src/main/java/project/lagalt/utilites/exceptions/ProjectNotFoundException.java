dbd043ba4c41bc74bb90c84f73bf7716a4e13089
98emre <emre-98@live.se>
2023-08-30 11:11:54 +0200

    feat: created exception for project

---
+++ src/main/java/project/lagalt/utilites/exceptions/ProjectNotFoundException.java
@@ -0,0 +1,14 @@
+package project.lagalt.utilites.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProjectNotFoundException extends RuntimeException {
+
+    public ProjectNotFoundException(int id){
+        super("Project with id: " + id + " does not exist");
+    }
+
+}
