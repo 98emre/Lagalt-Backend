dc845eafa29f2786b4a72e6637ee984f0fa53aa5
98emre <emre-98@live.se>
2023-08-30 11:34:19 +0200

    feat: created project mapper

---
+++ src/main/java/project/lagalt/mapper/ProjectMapper.java
@@ -0,0 +1,23 @@
+package project.lagalt.mapper;
+
+
+import org.mapstruct.Mapper;
+import project.lagalt.model.dtos.project.ProjectDTO;
+import project.lagalt.model.dtos.project.ProjectPostDTO;
+import project.lagalt.model.dtos.project.ProjectUpdateDTO;
+import project.lagalt.model.entities.Project;
+
+import java.util.Collection;
+
+@Mapper(componentModel = "spring")
+public abstract class ProjectMapper {
+
+    public abstract Project ProjectPostDtoTOProject(ProjectPostDTO projectPostDTO);
+
+    public abstract Project ProjectUpdateDtoTOProject(ProjectUpdateDTO projectUpdateDTO);
+
+    public abstract ProjectDTO ProjectToProjectDTO(Project project);
+
+    public abstract Collection<ProjectDTO> ProjectsToProjectDTO(Collection<Project> projects);
+
+}
