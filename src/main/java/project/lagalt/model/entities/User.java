5bc62620b765c389bf09038847c0bc9d2351ed08
98emre <emre-98@live.se>
2023-08-30 09:20:35 +0200

    fix: added more attribute for entity user

--- src/main/java/project/lagalt/model/entities/User.java
+++ src/main/java/project/lagalt/model/entities/User.java
@@ -21,6 +21,14 @@ public class User {
     @Column(name = "password", length = 50, nullable = false)
     private String password;
 
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+
+    @Column(name = "fullname", length = 50)
+    private String fullname;
+
     @Column(name = "skills")
     @Enumerated(EnumType.STRING)
     @ElementCollection
@@ -31,10 +39,12 @@ public class User {
 
     }
 
-    public User(int id, String username, String password, Set<Skills> skills) {
+    public User(int id, String username, String password, String email, String fullname, Set<Skills> skills) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.fullname = fullname;
         this.skills = skills;
     }
 
@@ -60,6 +70,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
     }
 
     public Set<Skills> getSkills() {
