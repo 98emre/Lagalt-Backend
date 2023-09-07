08334bfe5cb6cbfe38d58938cf96e9011d5c1dfb
98emre <emre-98@live.se>
2023-09-07 10:53:34 +0200

    feat: added findByProjectAndStatus på repositoty

--- src/main/java/project/lagalt/repository/CollaboratorRepository.java
+++ src/main/java/project/lagalt/repository/CollaboratorRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.lagalt.model.entities.Collaborator;
 import project.lagalt.model.entities.Project;
+import project.lagalt.utilites.enums.Application;
+
+import java.util.Set;
 
 @Repository
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Integer> {
+
+
+    Set<Collaborator> findByProjectAndStatus(Project project, Application status);
 
 }
