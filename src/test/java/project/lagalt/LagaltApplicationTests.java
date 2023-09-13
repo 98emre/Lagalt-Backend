0dd2236d006b949f964d3147243c4cb79f0ec8eb
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-09-13 10:20:18 +0200

    Fix broken (unnecessary?) tests. ATM they don't do anything, so remove.

--- src/test/java/project/lagalt/LagaltApplicationTests.java
+++ src/test/java/project/lagalt/LagaltApplicationTests.java
@@ -1,23 +1,14 @@
 package project.lagalt;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import project.lagalt.service.UserService;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest(classes = LagaltApplication.class)
 @ContextConfiguration
 class LagaltApplicationTests {
 
-	@Autowired
-	private UserService userService;
-
 	@Test
 	void contextLoads() {
-		assertNotNull(userService);
+		// TODO
 	}
 
 }
