f3abd69108b787e0da027cb958b7645f2a0ba3b9
98emre <emre-98@live.se>
2023-09-15 11:42:46 +0200

    feat: added dtos for message response

---
+++ src/main/java/project/lagalt/model/dtos/messageresponse/MessageResponseUpdateDTO.java
@@ -0,0 +1,34 @@
+package project.lagalt.model.dtos.messageresponse;
+
+import java.time.LocalDate;
+
+public class MessageResponseUpdateDTO {
+
+    private int id;
+    private String responseText;
+    private LocalDate date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public void setResponseText(String responseText) {
+        this.responseText = responseText;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
